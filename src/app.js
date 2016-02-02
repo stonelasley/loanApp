e8117ee4899eee15e0c166187fb05250e531baff
Stone C. Lasley <stonelasley@gmail.com>
2016-02-01 22:12:24 -0700

    beginning app with ui-router structure

---
+++ src/app.js
@@ -0,0 +1,32 @@
+angular.module('applicationApp', ['ngAnimate', 'ui.router'])
+
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+      .state('application', {
+        url: '/application',
+        templateUrl: 'application.html',
+        controller: 'applicationController'
+      })
+
+      .state('application.car', {
+        url: '/car',
+        templateUrl: 'application-car.html'
+      })
+
+    $urlRouterProvider.otherwise('/application/car');
+  })
+
+  .controller('applicationController', function($scope) {
+
+    $scope.formData = {};
+
+    $scope.processForm = function() {
+
+      console.log('APPLICATION SUBMISSION');
+      alert($scope.formData);
+
+    };
+
+  });
